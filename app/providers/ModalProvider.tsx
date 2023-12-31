be9f739b6eb7af9e5a71182dc20eebf28ed86b82
Saumyapratim Das <saumyadas2017@gmail.com>
2023-12-31 10:52:02 +0530

    Edit

--- app/providers/ModalProvider.tsx
+++ app/providers/ModalProvider.tsx
@@ -1,14 +1,8 @@
 import getCurrentUser from "../actions/getCurrentUser";
-import getEventById from "../actions/getEventById";
-import CreateModal from "../components/modals/CreateModal";
-import EmailModal from "../components/modals/EmailModal";
-import LikesModal from "../components/modals/LikesModal";
 import LoginModal from "../components/modals/LoginModal";
 import RegisterModal from "../components/modals/RegisterModal";
 import SuccessModal from "../components/modals/SuccessModal";
-import WelcomeModal from "../components/modals/WelcomeModal";
-import RegistrationSuccessModal from "../events/[eventId]/RegistrationSuccessModal";
-// import EventRegistrationModal from "../components/modals/event registration/EventRegistrationForm";
+
 import { SafeUser } from "../types";
 
 const ModalProvider = async () => {
@@ -16,13 +10,9 @@ const ModalProvider = async () => {
     
     return ( 
         <div>
-            <EmailModal/>
-            <CreateModal currentUser={currentUser}/>
-            <WelcomeModal/>
             <SuccessModal/>
             <LoginModal/>
             <RegisterModal/>
-            <RegistrationSuccessModal/>
         </div>
      );
 }
