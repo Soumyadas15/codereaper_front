b3e79451b1ed134a507f9a778f23e1843ecc8321
Saumyapratim Das <saumyadas2017@gmail.com>
2023-12-31 11:01:27 +0530

    prisma issue

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,9 +1,6 @@
 import { User } from "@prisma/client";
 
-export type SafeUser = Omit<
-  User,
-  "createdAt" | "updatedAt"
-> & {
+export type SafeUser = Omit< User, "createdAt" | "updatedAt" > & {
   createdAt: string;
   updatedAt: string;
 };
